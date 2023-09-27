8a5fa4bf11473a8c1d5d562fd7574af2b32043d3
RafaelFacchin <rafael.facchin@yahoo.com.br>
2023-09-27 18:18:20 -0300

    CHANGIN COLOR BOX

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace _2_Fun_if_else
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(233, 33);
+            checkBox1.Margin = new Padding(3, 4, 3, 4);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(176, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Enable color changing";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Location = new Point(14, 16);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 57);
+            button1.TabIndex = 1;
+            button1.Text = "Change the color if the box is checked\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Red;
+            label1.Font = new Font("Segoe UI Symbol", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(14, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(351, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Press the button to change my color";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(648, 157);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Font = new Font("Segoe UI Symbol", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2_Fun_if_else
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if ((checkBox1.Checked) == true)
+            {
+                if (label1.BackColor == Color.Red)
+                {
+                    label1.BackColor = Color.Blue;
+                }
+                else
+                {
+                    label1.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                MessageBox.Show("The box is not checked!", "Notice");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                for (int c = 0; c < 254 && Visible; c++)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+
+                for (int c = 254; c >= 0 && Visible; c--)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //Text("Press the button to change my color");
+        }
+    }
+}
